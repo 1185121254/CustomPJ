f205e02f793d9fab90903d8b6c1a3766c760ea48
chaojie <chaojie@chaojiedemac-mini.local>
2017-06-01 16:28:33 +0800

    Initial Commit

---
+++ CustomPJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomPJ
+//
+//  Created by chaojie on 2017/6/1.
+//  Copyright © 2017年 chaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomPJ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomPJ
+//
+//  Created by chaojie on 2017/6/1.
+//  Copyright © 2017年 chaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
